c7d8847df8304603ebe035222453501215215332
Thomas Ngo <thomasngo818@gmail.com>
2022-02-02 04:39:37 +0000

    Add code to event handler to change the counter color if it is negative.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
 
+  // handler to update char counter based on text input length
   const textInput = $("#tweet-text");
   textInput.keyup(function() {
+
     const counter = $(this).parent().find(".counter");
     counter.text(140 - $(this).val().length);
 
+    // change counter color if negative
+    if (counter.text()[0] === '-') {
+      counter.css("color", "red");
+    } else {
+      counter.css("color", "");
+    }
 
   });
 

9e5a2eb30fb972b98ccb7c8e902dc1d1a1019741
Thomas Ngo <thomasngo818@gmail.com>
2022-02-03 05:13:37 +0000

    Add implementation of renderTweet function.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -40,6 +40,10 @@ $(document).ready(() => {
   // takes an array of tweet objects and appends them to the tweet container
   const renderTweets = function(tweets) {
     // loop through tweets
+    for (const tweet of data) {
+      const $tweet = createTweetElement(tweet);
+      $('.container').append($tweet);
+    }
   }
 
 
@@ -56,9 +60,36 @@ $(document).ready(() => {
     "created_at": 1461116232227
   }
 
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
   const $tweet = createTweetElement(tweetData);
   console.log('tweet:', $tweet.get(0));
-  $('.container').append($tweet);
+  // $('.container').append($tweet);
+  renderTweets(data);
 });
 
 {/* <article class="tweet">

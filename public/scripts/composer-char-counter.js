a880667fb269f26ebaaa594c33b65776c0b397fd
Thomas Ngo <thomasngo818@gmail.com>
2022-02-04 07:48:30 +0000

    Fix counter handler to capture all inputs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   // handler to update char counter based on text input length
   const textInput = $("#tweet-text");
-  textInput.keyup(function() {
+  textInput.on('input', function() {
 
     const counter = $(this).parent().find(".counter");
     counter.text(140 - $(this).val().length);
